1295a18d0f6a020435afde6c285448c2bd97c68d
Emily C <emilymargaretcain@gmail.com>
2016-11-09 13:14:56 -0800

    added secrets_template

---
+++ emcain_django/secrets_template.py
@@ -0,0 +1,10 @@
+# copy as secrets.py (do not check into repo) and fill in secret key/paszword vars
+
+AWS_PUBLIC_KEY = ''
+AWS_SECRET_KEY = ''
+
+RECAPTCHA_PUBLIC_KEY = ''
+RECAPTCHA_SECRET_KEY = ''
+
+EMAIL_CLIENT_PUBLIC_KEY = ''
+EMAIL_CLIENT_PUBLIC_KEY = ''
